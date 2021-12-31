86e3cbf14f51d9ea75230f9bc1f784ba0a268913
AdamLui <wingfului@gmail.com>
2021-12-31 15:19:34 -0800

    side-by-side, more responsive to desktop, and fix images

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Head from 'next/head';
 import NavigationBar from './NavigationBar';
+import { prefix } from '../utils';
 
 interface HeaderProps {
     titleName?: string;
@@ -15,7 +16,7 @@ const Header = (props : HeaderProps) => {
             <Head>
                 <title>Lui Finance App {titleSuffix} </title>
                 <meta name="description" content={metadataContent} />
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="icon" href={`${prefix}/favicon.ico`} />
             </Head>
             <NavigationBar/>
         </React.Fragment>
