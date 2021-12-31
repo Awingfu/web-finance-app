b4ec40200688f693ca285b9f1a7b19bb2df04a42
AdamLui <wingfului@gmail.com>
2021-12-30 23:11:17 -0800

    deployment fixes for navigation

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const path = require('path')
 
 module.exports = {
+  assetPrefix: './',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
