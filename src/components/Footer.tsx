b13779c647b431492599fa669a686ffdfb32a1c9
AdamLui <wingfului@gmail.com>
2021-12-31 22:36:25 -0800

    create new retirment styles, add stock purchase to paycheck, add github link to footer, minor fixes, secondary color to check marks

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,11 @@
 import React from "react";
- 
+import { SocialIcon } from 'react-social-icons';
+
 const Footer = () => {
- return (
+  return (
     <footer>
         <p>Created by: Adam Lui</p>
+        <SocialIcon url="https://github.com/Awingfu/web-finance-app" bgColor="transparent" fgColor="black" target="_blank" rel="noreferrer noopener" />
     </footer>
   );
 };
