fc0782b347c2c494d085050b18cbff331eace320
Adam Lui <wingfului@gmail.com>
2022-12-20 17:09:00 -0600

    test2

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,58 +1,65 @@
 import React from "react";
-import Link from 'next/link';
-import Image from 'next/image';
-import { Container, Nav, Navbar} from 'react-bootstrap';
-import { prefix } from '../utils';
+import Link from "next/link";
+import Image from "next/image";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { prefix } from "../utils";
 
 /**
- * Navbar items are for CSS, we have to wrap in next/link in order for single page behavior 
+ * Navbar items are for CSS, we have to wrap in next/link in order for single page behavior
  * @returns NavigationBar JSX
  */
 const NavigationBar = () => {
+  const [isGreaterThan425px, setIsGreaterThan425px] = React.useState(false);
+  const iconPath = prefix + "/favicon.ico";
 
-    const [isGreaterThan425px, setIsGreaterThan425px] = React.useState(false);
-    const iconPath = prefix + '/favicon.ico';
+  React.useEffect(() => {
+    function handleResize() {
+      if (window.innerWidth > 425) {
+        setIsGreaterThan425px(true);
+      } else {
+        setIsGreaterThan425px(false);
+      }
+    }
 
-    React.useEffect(() => {
-        function handleResize() {
-        if (window.innerWidth > 425) {
-            setIsGreaterThan425px(true);
-        } else {
-            setIsGreaterThan425px(false);
-        }
-        }
+    handleResize();
 
-        handleResize();
+    window.addEventListener("resize", handleResize);
 
-        window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
-        return () => window.removeEventListener("resize", handleResize);
-    }, []);
-
-    return (
-        <Navbar bg="primary" variant="dark">
-            <Container>
-                <Link href="/" passHref>
-                    <Navbar.Brand> {
-                        isGreaterThan425px ? "Finance App" : 
-                        <Image src={iconPath} alt="Finance App Icon" width={30} height={30}/>
-                    }</Navbar.Brand>
-                    <div></div>
-                </Link>
-                <Nav className="me-auto">
-                    <Link href="/paycheck" passHref>
-                        <Nav.Link> Paycheck </Nav.Link>
-                    </Link>
-                    <Link href="/retirement/maximize" passHref>
-                        <Nav.Link> {isGreaterThan425px && "401k " } Maximizer </Nav.Link>
-                    </Link>
-                    <Link href="/retirement/frontload" passHref>
-                        <Nav.Link> {isGreaterThan425px && "401k " } Frontloader </Nav.Link>
-                    </Link>
-                </Nav>
-            </Container>
-        </Navbar>
-    );
+  return (
+    <Navbar bg="primary" variant="dark">
+      <Container>
+        <Link href="/" passHref>
+          <Navbar.Brand>
+            {" "}
+            {isGreaterThan425px ? (
+              "Finance App"
+            ) : (
+              <Image
+                src={iconPath}
+                alt="Finance App Icon"
+                width={30}
+                height={30}
+              />
+            )}
+          </Navbar.Brand>
+        </Link>
+        <Nav className="me-auto">
+          <Link href="/paycheck" passHref>
+            <Nav.Link> Paycheck </Nav.Link>
+          </Link>
+          <Link href="/retirement/maximize" passHref>
+            <Nav.Link> {isGreaterThan425px && "401k "} Maximizer </Nav.Link>
+          </Link>
+          <Link href="/retirement/frontload" passHref>
+            <Nav.Link> {isGreaterThan425px && "401k "} Frontloader </Nav.Link>
+          </Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
 };
 
 export default NavigationBar;
