d47f22c81b2321bd54a0bbdb4dc324f20e7079ec
Adam Lui <wingfului@gmail.com>
2022-12-20 23:14:18 -0600

    Fix image during next build

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-  assetPrefix: './',
+  assetPrefix: "./",
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+    includePaths: [path.join(__dirname, "styles")],
   },
   reactStrictMode: true,
-}
+  images: {
+    unoptimized: true,
+  },
+};
--- src/utils/federal_withholding.ts
+++ src/utils/federal_withholding.ts
@@ -2,6 +2,7 @@
  * Latest updated year: 2023
  * Federal Withholding Source: https://www.irs.gov/publications/p15t Manual Payroll Systems W4 after 2020
  * Federal Withholding Source PDF: https://www.irs.gov/pub/irs-dft/p15t--dft.pdf
+ * W4 Reference for math checks: https://www.irs.gov/pub/irs-pdf/fw4.pdf
  * FICA and Medicare Tax Source: https://www.nerdwallet.com/article/taxes/fica-tax-withholding
  * Additional Medicare Tax Source https://www.irs.gov/taxtopics/tc560
  * This file contains Federal, FICA (Social Security), and Medicare withholding
