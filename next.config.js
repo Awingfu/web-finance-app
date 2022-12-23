1e283820779eca44359d83dc8b7e40838c39176b
Adam Lui <wingfului@gmail.com>
2022-12-22 22:53:17 -0600

    add env in preview yml

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const path = require("path");
 
 module.exports = {
-  assetPrefix: "./",
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
   sassOptions: {
