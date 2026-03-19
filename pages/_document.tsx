78a6892248ae2850aab380d28d1a8292ebe07138
Adam Lui <wingfului@gmail.com>
2026-03-18 20:09:26 -0500

    adding og image for web scrappers

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,6 +11,25 @@ export default function Document() {
     <Html>
       <Head>
         <script dangerouslySetInnerHTML={{ __html: themeScript }} />
+        <meta property="og:title" content="Web Finance App" />
+        <meta
+          property="og:description"
+          content="Free calculators for paycheck, retirement, and investing"
+        />
+        <meta
+          property="og:image"
+          content="https://adamwlui.com/web-finance-app/og-image.png"
+        />
+        <meta
+          property="og:url"
+          content="https://adamwlui.com/web-finance-app"
+        />
+        <meta property="og:type" content="website" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:image"
+          content="https://adamwlui.com/web-finance-app/og-image.png"
+        />
       </Head>
       <body>
         <Main />
